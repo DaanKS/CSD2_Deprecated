ff53c3237b746631e450832b4e8b61b60908f4e4
DaanKS <daan1995@live.co.uk>
2021-02-24 16:28:13 +0100

    Added Working Pitchshifter

---
+++ CSD2c/Tests/PitchShiftTest2/Saw.cpp
@@ -0,0 +1,38 @@
+#include "Saw.h"
+#include <cmath>
+//constructor destructor
+Saw::Saw(double frequency, double samplerate)
+{
+  this->samplerate = samplerate;
+  this->frequency = frequency;
+}
+
+Saw::~Saw()
+{}
+
+//Methods
+//Ignoring protection for now
+void Saw::setFrequency(double frequency)
+{
+  this->frequency = frequency;
+}
+
+double Saw::getFrequency()
+{
+  return frequency;
+}
+//getSample and tick
+double Saw::getSample()
+{
+  return sampletje;
+}
+
+void Saw::tick()
+{
+  //This calculation was given to me by Bas Maat
+  phase += frequency / samplerate;
+  // wrap phase from 0 to 1
+  if(phase >= 1) phase = phase - 1;
+  //Scaling
+  sampletje = (phase * 100.) + 5.;
+}
